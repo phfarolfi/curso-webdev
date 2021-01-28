d28f9ab1cf00b4256f722520be498ae7d05bbbea
Pedro Farolfi <phfarolfi@gmail.com>
2021-01-28 12:49:22 -0300

    Terminei o projeto 10
    Terminei o projeto 10, onde criei um jogo de point-and-click para praticar os conceitos da linguagem javascript.

--- secao-javascript/projeto10/jogo.js
+++ secao-javascript/projeto10/jogo.js
@@ -2,33 +2,82 @@
 var altura = window.innerHeight
 var largura = window.innerWidth
 
+// Variável que representa qual vida vai ser removida na função posicaoAleatoria()
+var vidas = 1
+
+// Variáveis que controlam o tempo de jogo e o tempo que o mosquito fica na tela
+var tempo_mosquito = 1500
+var tempo = 10
+
+// Variável que recebe o nível do jogo
+var nivel = window.location.search
+nivel = nivel.replace('?', '')
+
+if(nivel === 'normal'){
+    tempo_mosquito = 1500
+    tempo = 15
+} else if (nivel === 'dificil'){
+    tempo_mosquito = 1000
+    tempo = 20
+} else if (nivel === 'chucknorris'){
+    tempo_mosquito = 750
+    tempo = 30
+}
+
+var cronometro = setInterval(function(){
+    tempo--
+
+    if(tempo < 0){
+        window.location.href = 'vitoria.html'
+    }
+
+    document.getElementById('cronometro').innerHTML = tempo
+}, 1000)
+
 function AjustarJanela() {
     altura = window.innerHeight
     largura = window.innerWidth
-
-    console.log(largura, altura)
 }
 
-
-// Criar o elemento mosquito
+// Criar o elemento mosquito na tela
 function posicaoAleatoria(){
+    //Remove o mosquito anterior (caso exista)
+    if(document.getElementById('mosquito')) {
+        document.getElementById('mosquito').remove()
+
+        if(vidas > 3){
+             window.location.href = 'derrota.html'
+        }
+
+        document.getElementById('v' + vidas).src = "imagens/coracao_vazio.png"
+        vidas++
+    }
+
     // Utilizar a função Math.random() para gerar valores aleatórios para os mosquitos
     var posicaoX = Math.floor(Math.random()*largura) - 100
     var posicaoY = Math.floor(Math.random()*altura) - 100
 
+    //Impede que o mosquito apareça fora da tela
     posicaoX = posicaoX < 0 ? 0 : posicaoX
     posicaoY = posicaoY < 0 ? 0 : posicaoY
 
-    console.log(posicaoX, posicaoY)
-
+    //Cria o elemento mosquito em uma posição aleatória da tela
     var mosquito = document.createElement('img')
     mosquito.src = 'imagens/mosquito.png'
-    mosquito.className = 'mosquito1'
     mosquito.style.left = posicaoX + 'px'
     mosquito.style.top = posicaoY + 'px'
     mosquito.style.position = 'absolute'
-    mosquito.style.width = Math.floor(Math.random()*70) + 'px'
-    mosquito.style.height = Math.floor(Math.random()*70) + 'px'
+    mosquito.id = 'mosquito'
+    mosquito.onclick = function(){
+        this.remove()
+    }
 
+    // Variáveis criadas para escolher aleatoriamente as classes do mosquito (pequeno/médio/grande) e (olhando pra esquerda/olhando para a direita)
+    var classe = Math.floor(Math.random()*3) + 1
+    var lado = Math.floor(Math.random()*2) + 1
+    lado = lado == 1 ? 'ladoA' : 'ladoB'
+    mosquito.className = 'mosquito' + (classe) + ' ' + (lado)
+
+    //Mostra o mosquito na tela
     document.body.appendChild(mosquito)
 }
