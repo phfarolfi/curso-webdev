0035dccc48c3f22ce5968312bca81b173d4ea6df
Pedro Farolfi <phfarolfi@gmail.com>
2021-02-15 18:28:28 -0300

    Projeto 12 - Update
    Tornei a abertura de chamado e a consulta de chamado funcionais. Criei o arquivo que armazena os chamados e implementei uma solução para mostrar os registros de cada usuário somente para ele mesmo ou para um administrador do sistema.

--- backend/secao-php/projeto12/App Help Desk/valida_login.php
+++ backend/secao-php/projeto12/App Help Desk/valida_login.php
@@ -3,21 +3,31 @@
     
     // Variável de autenticação
     $usuario_autenticado = false;
+    $usuario_id = null;
+    $usuario_perfil = null;
+
+    $perfis = array(1 => 'ADM', 2=> 'USER');
 
     // Usuários do sistema
     $usuarios_app = array(
-        array('email' => 'adm@teste.com.br', 'senha' => '123456'),
-        array('email' => 'user@teste.com.br', 'senha' => 'abcd')
+        array('id' => 1,'email' => 'adm@teste.com.br', 'senha' => '1234', 'perfil_id' => 1),
+        array('id' => 2,'email' => 'user@teste.com.br', 'senha' => '1234', 'perfil_id' => 1),
+        array('id' => 3,'email' => 'jose@teste.com.br', 'senha' => '1234', 'perfil_id' => 2),
+        array('id' => 4,'email' => 'maria@teste.com.br', 'senha' => '1234', 'perfil_id' => 2)
     );
 
     foreach($usuarios_app as $user) {
         if($user['email'] == $_POST['email'] && $user['senha'] == $_POST['senha']) {
             $usuario_autenticado = true;
+            $usuario_id = $user['id'];
+            $usuario_perfil = $user['perfil_id'];
         }
     }
 
     if($usuario_autenticado) {
         $_SESSION['autenticado'] = 'SIM';
+        $_SESSION['id'] = $usuario_id;
+        $_SESSION['perfil_id'] = $usuario_perfil;
         header('Location: home.php');
     }
     else {
